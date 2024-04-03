f922bfe9686d12d560a31087f91cc9581f3d88f6
JeisonOJ <jeisonortiz1516@gmail.com>
2024-04-03 11:47:01 -0500

    entities created

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
-        Connection connection = ConfigDB.openConnection();
-        ConfigDB.closeConnection();
+
     }
 
 
