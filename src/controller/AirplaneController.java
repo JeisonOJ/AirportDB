cfad4c4efab7ad3a3ed56264659799405dc33070
JeisonOJ <jeisonortiz1516@gmail.com>
2024-04-03 13:41:44 -0500

    airplaneController

--- src/controller/AirplaneController.java
+++ src/controller/AirplaneController.java
@@ -1,4 +1,19 @@
 package controller;
 
+import model.AirplaneModel;
+
 public class AirplaneController {
+
+    public static AirplaneModel instanceModel(){
+        return new AirplaneModel();
+    }
+
+    public static String showAllAirplanes(){
+        StringBuilder message = new StringBuilder();
+        message.append("");
+
+        instanceModel().findAll();
+        return message.toString();
+    }
+
 }
